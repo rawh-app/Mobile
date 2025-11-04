ca76a6dd4511b19c889e39428d163262af5c0f64
osama emad <oemad7155@gmail.com>
2025-11-04 21:36:56 +0200

    adds1244

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:   Container(child: Center(child: Text('data now122'),),),
+      home:   Container(child: Center(child: Text('data now12'),),),
     );
   }
 }
